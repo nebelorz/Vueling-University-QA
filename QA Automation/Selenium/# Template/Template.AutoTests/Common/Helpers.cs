a431a0e0e16063754ddc074937cb5447aab7c2ea
Alex Gassó <alex.gasso@col.vueling.com>
2023-06-08 00:29:43 +0200

    update Selenium template

--- QA Automation/Selenium/# Template/Template.AutoTests/Common/Helpers.cs
+++ QA Automation/Selenium/# Template/Template.AutoTests/Common/Helpers.cs
@@ -5,41 +5,34 @@ namespace Template.Auto.Common {
 
     internal class Helpers {
         public const string CORRESPONDENCIA = "TRWAGMYFPDXBNJZSQVHLCKE";
-        public const string Test = "TEST";
+        public const string TEST = "TEST";
 
         public static char GetRandomLetter() {
-            string seats = "ABCDEF";
+            const string letters = "abcdefghijklmnopqrstuvwxyz";
             Random random = new Random();
-            int num = random.Next(0, seats.Length - 1);
-            return seats[num];
-        }
-
-        public static int GetRandomDay() {
-            Random random = new Random();
-            int randomDay = random.Next(1, 31);
-            return randomDay;
+            int num = random.Next(0, letters.Length - 1);
+            return letters[num];
         }
 
         public static string GetRandomString(int length) {
             const string letters = "abcdefghijklmnopqrstuvwxyz";
             Random random = new Random();
-            var chars = Enumerable.Range(0, length)
-                .Select(x => letters[random.Next(0, letters.Length)]);
+            var chars = Enumerable.Range(0, length).Select(x => letters[random.Next(0, letters.Length)]);
             return new string(chars.ToArray());
         }
 
-        public static char GetNifLetter(string dni) {
+        public static char GetDNILetter(string dni) {
             int n;
 
             if ((dni == null) || (!int.TryParse(dni.Substring(0, 8), out n)))
             {
-                throw new ArgumentException("Dni should be 8 digits");
+                throw new ArgumentException("DNI should be 8 digits");
             }
 
             return CORRESPONDENCIA[n % 23];
         }
 
-        public static string GetRandomDniDigits() {
+        public static string GetRandomDNIDigits() {
             const string digits = "0123456789";
             Random random = new Random();
             var chars = Enumerable.Range(0, 8)
@@ -47,9 +40,9 @@ namespace Template.Auto.Common {
             return new string(chars.ToArray());
         }
 
-        public static string GetRandomDni() {
-            string dniDigits = GetRandomDniDigits();
-            char dniLetter = GetNifLetter(dniDigits);
+        public static string GetRandomDNI() {
+            string dniDigits = GetRandomDNIDigits();
+            char dniLetter = GetDNILetter(dniDigits);
             string dni = dniDigits + dniLetter.ToString();
             return dni;
         }
@@ -61,14 +54,18 @@ namespace Template.Auto.Common {
         }
 
         public static string GenerateFirstName(int length) {
-            string FirstName = "";
-            FirstName = Test + GetRandomString(length);
+            string FirstName = TEST + GetRandomString(length);
             return FirstName;
         }
 
         public static string GenerateLastName(int length) {
-            string LastName = Test + GetRandomString(length);
+            string LastName = TEST + GetRandomString(length);
             return LastName;
+        }
+
+        public static string GenerateEmail() {
+            string email = $"{TEST}_{GetRandomString(6)}@test.com";
+            return email;
         }
 
         public static int GetRandomPhoneNumber() {
