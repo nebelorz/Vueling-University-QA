9926da56161a478d97b831c07aed21bef3eb4216
Alex Gassó <alex.gasso@col.vueling.com>
2023-06-15 18:55:32 +0200

    flylevel tests completed w/ report

--- QA Automation/Cypress/Flylevel Auto Tests/cypress/e2e/SearcherTests.cy.js
+++ QA Automation/Cypress/Flylevel Auto Tests/cypress/e2e/SearcherTests.cy.js
@@ -2,6 +2,13 @@ import { HomePage } from "../webpages/HomePage";
 
 describe("Searcher Tests", () => {
   const homePage = new HomePage();
+  let data;
+
+  before(() => {
+    cy.fixture("flightData").then((jsonData) => {
+      data = jsonData;
+    });
+  });
 
   beforeEach(() => {
     cy.visit("", {
@@ -14,7 +21,21 @@ describe("Searcher Tests", () => {
 
   it("OW BCN-EZE 2ADT 1INF", () => {
     homePage.acceptCookies();
-    //homePage.selectOW();
-    //homePage.fillOriginAndDestination();
+    homePage.fillOriginAndDestination(data.origin, data.destination);
+    // cy.screenshot("SS_AFTER_FLIGHT_SELECTION");
+    homePage.searcher.searcherContainer().screenshot("SS_AFTER_FLIGHT_SELECTION");
+    homePage.switchTripToOW();
+    homePage.selectMonthInCalendar(data.month);
+    // cy.screenshot("SS_AFTER_DATEPICKER_SELECTION");
+    homePage.searcher.searcherContainer().screenshot("SS_AFTER_DATEPICKER_SELECTION");
+    homePage.selectFirstDayAvailable();
+    homePage.selectPax(data.adults, data.childs, data.infants);
+    // cy.screenshot("SS_AFTER_PAX_SELECTION");
+    homePage.searcher.searcherContainer().screenshot("SS_AFTER_PAX_SELECTION");
+    homePage.submitSearch();
   });
+
+  after(() => {
+    cy.screenshot("SS_TEST_FINISHED")
+  })
 });
--- QA Automation/Cypress/Flylevel Auto Tests/cypress/webpages/HomePage.js
+++ QA Automation/Cypress/Flylevel Auto Tests/cypress/webpages/HomePage.js
@@ -1,34 +1,107 @@
+import { paxEnum } from "../support/enums";
+
 export class HomePage {
   // Elements
   closeNewsletter = () => cy.get('div [data-fl-track="close-screen-login"]');
   btnAcceptCookies = () => cy.get("#ensCloseBanner");
 
   searcher = {
-    travelType: () => cy.get(".dropdown-title-js"),
+    searcherContainer: () => cy.getId("searcher-block"),
+    travelType: () => cy.get('[data-target="dropdown-trip"]'),
     travelTypeOW: () => cy.getId("dropdown-trip"),
     origin: () => cy.get('[data-field="origin"]'),
+    originList: () => cy.get(".origin .group-info:not(.hidden)"),
     destination: () => cy.get('[data-field="destination"]'),
+    destinationList: () => cy.get(".destination .group-info:not(.hidden)"),
     departureDate: () => cy.get(".departure-date .input-value"),
+    paxDropdown: () => cy.get(".searcher-passengers"),
+    btnSubmit: () => cy.get("#searcher_submit_buttons"),
   };
 
-  datepicker = {};
+  datepicker = {
+    switchTripToOW: () => cy.get('.switch-position [value="RT"]'),
+    switchTripToRT: () => cy.get('.switch-position [value="OW"]'),
+    months: () => cy.get("span.month"),
+    arrowNextMonth: () => cy.get("button.datepicker__next-action"),
+    availableDays: () => cy.get(".is-available:not(.is-previous-month"),
+  };
+
+  paxMenu = {
+    paxCount: (pax) => cy.get(`[data-field="${pax}"] .js-pax-count`),
+    paxPlusIcon: (pax) => cy.get(`[data-field="${pax}"] .js-plus`),
+    btnSubmit: () => cy.get("button.btn-pax"),
+  };
 
   // Methods
   acceptCookies() {
     this.btnAcceptCookies().should("be.visible");
     this.btnAcceptCookies().click();
-    cy.get("#nav-logo").scrollIntoView();
   }
 
-  selectOW() {
-    cy.get("#nav-logo").scrollIntoView();
-    this.searcher.travelType().click();
-    this.searcher.travelTypeOW().click();
+  fillOriginAndDestination(origin, destination) {
+    this.searcher.origin().type(origin);
+    this.searcher.originList().first().should("be.visible");
+    this.searcher.originList().first().click();
+
+    this.searcher.destination().type(destination);
+    this.searcher.destinationList().first().should("be.visible");
+    this.searcher.destinationList().first().click();
   }
 
-  fillOriginAndDestination() {
-    cy.fixture("flightData").then((data) => {
-      this.searcher.origin().type(data.origin);
+  switchTripToOW() {
+    this.datepicker.switchTripToOW().click();
+    Cypress.on("uncaught:exception", (err, runnable) => {
+      return false;
     });
+  }
+
+  selectMonthInCalendar(month) {
+    this.datepicker.months().should("be.visible");
+
+    this.datepicker
+      .months()
+      .invoke("text")
+      .then((monthName) => {
+        Cypress.on("uncaught:exception", (err, runnable) => {
+          return false;
+        });
+
+        if (monthName !== month) {
+          this.datepicker.arrowNextMonth().click();
+          return this.selectMonthInCalendar(month);
+        }
+
+        cy.scrollTo("top");
+      });
+  }
+
+  selectFirstDayAvailable() {
+    this.datepicker.availableDays().first().click();
+  }
+
+  addPax(pax, number) {
+    this.paxMenu.paxCount(pax).then((paxElement) => {
+      const paxNumber = paxElement.text();
+
+      if (paxNumber !== number) {
+        this.paxMenu.paxPlusIcon(pax).first().click();
+        this.addPax(pax, number);
+      }
+    });
+  }
+
+  selectPax(numAdults, numChilds, numInfants) {
+    this.searcher.paxDropdown().click();
+    this.addPax(paxEnum.Adult, numAdults);
+    this.paxMenu.paxCount(paxEnum.Adult).should("have.text", numAdults);
+    this.addPax(paxEnum.Child, numChilds);
+    this.paxMenu.paxCount(paxEnum.Child).should("have.text", numChilds);
+    this.addPax(paxEnum.Infant, numInfants);
+    this.paxMenu.paxCount(paxEnum.Infant).should("have.text", numInfants);
+    this.paxMenu.btnSubmit().click();
+  }
+
+  submitSearch() {
+    this.searcher.btnSubmit().click();
   }
 }
